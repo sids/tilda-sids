f4babf2ab864a9e3ecdc8502643dd2aae7d017d1
Siddhartha Reddy <sids@grok.in>
2012-08-12 17:17:00 +0530

    Aquamacs preferences.

---
+++ Library/Preferences/Aquamacs Emacs/Preferences.el
@@ -0,0 +1,9 @@
+;; This is the Aquamacs Preferences file.
+;; Add Emacs-Lisp code here that should be executed whenever
+;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
+;; evaluating this file and print errors in the *Messags* buffer.
+;; Use this file in place of ~/.emacs (which is loaded as well.)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(setq ns-pop-up-frames nil)
