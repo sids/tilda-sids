8f3e61587faf1165fdd8e50631bcbdfa9b8f2f17
Siddhartha Reddy <sids@grok.in>
2012-08-23 00:01:40 +0530

    Changing the keyboard shortcut for peepopen-got-file-project-gui.

--- .emacs.d/setup/peepopen.el
+++ .emacs.d/setup/peepopen.el
@@ -1,4 +1,4 @@
 (require 'peepopen-standalone)
 
 (global-set-key (kbd "M-o") 'peepopen-goto-file-gui)
-(global-set-key (kbd "M-p") 'peepopen-goto-file-in-project-gui)
+(global-set-key (kbd "C-x M-o") 'peepopen-goto-file-in-project-gui)
