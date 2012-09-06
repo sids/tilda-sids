ce873bd949c375731b439bf25a2dc8520c3188b6
Siddhartha Reddy <sids@grok.in>
2012-09-06 15:45:27 +0530

    Changing project root start for peepopen.

--- .emacs.d/setup/peepopen.el
+++ .emacs.d/setup/peepopen.el
@@ -1,4 +1,6 @@
 (require 'peepopen-standalone)
 
+(setq *peepopen-project-root-start* "~/src")
+
 (global-set-key (kbd "M-o") 'peepopen-goto-file-gui)
 (global-set-key (kbd "C-x M-o") 'peepopen-goto-file-in-project-gui)
