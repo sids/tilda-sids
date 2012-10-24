6134ac79ba16f8b164d009d5751906b4739c2f97
Siddhartha Reddy <sids@grok.in>
2012-10-24 15:24:31 +0530

    Switching to defthemes.

--- .emacs.d/setup/appearance.el
+++ .emacs.d/setup/appearance.el
@@ -2,5 +2,12 @@
 ;; Appearance
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(load-library "my-color-theme")
-(my-color-theme)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'cyberpunk t)
+
+(setq mac-allow-anti-aliasing 1)
+(set-face-attribute 'default nil
+                    :family "DejaVu Sans Mono" :height 115 :weight 'normal)
+
+;;(load-library "my-color-theme")
+;;(my-color-theme)
