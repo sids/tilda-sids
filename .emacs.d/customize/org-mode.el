1e800b76a33a7951b6e6c8fe759cd4ca92080edb
Siddhartha Reddy <sids@grok.in>
2012-10-25 22:14:57 +0530

    Minor changes in org-mode config.

--- .emacs.d/customize/org-mode.el
+++ .emacs.d/customize/org-mode.el
@@ -2,7 +2,9 @@
 ;; org-mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
 (global-set-key "\C-cb" 'org-iswitchb)
