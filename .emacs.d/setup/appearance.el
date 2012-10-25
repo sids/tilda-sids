1b136b0db133b5d83afe1d430e126bea25614c03
Siddhartha Reddy <sids@grok.in>
2012-10-26 00:00:21 +0530

    Customizing some faces.

--- .emacs.d/setup/appearance.el
+++ .emacs.d/setup/appearance.el
@@ -5,9 +5,20 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'cyberpunk t)
 
+(set-face-background 'ac-completion-face nil)
+(set-face-background 'hl-line-face "#1a1a1a")
+(defcustom highlight-symbol-colors
+  '("#2b2b2b" "#383838" "#4f4f4f" "#5f5f5f" "#6f6f6f")
+  "*Colors used by `highlight-symbol-at-point'.
+highlighting the symbols will use these colors in order."
+  :type '(repeat color)
+  :group 'highlight-symbol)
+
+
 (setq mac-allow-anti-aliasing 1)
 (set-face-attribute 'default nil
                     :family "DejaVu Sans Mono" :height 115 :weight 'normal)
+
 
 ;;(load-library "my-color-theme")
 ;;(my-color-theme)
