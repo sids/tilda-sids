1147e4af298a570e0d5f36939f1620472e9cbaf4
Siddhartha Reddy <sids@grok.in>
2012-10-26 17:28:16 +0530

    Customizing appearance.

--- .emacs.d/setup/appearance.el
+++ .emacs.d/setup/appearance.el
@@ -5,8 +5,29 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'cyberpunk t)
 
+(set-face-attribute 'default nil
+                    :family "DejaVu Sans Mono" :height 115 :weight 'normal)
+;;(setq mac-allow-anti-aliasing 1)
+
+(set-background-color "#1a1a1e")
+
+(setq-default cursor-type 'bar)
+(set-cursor-color "Red")
+
+(require 'auto-complete)
 (set-face-background 'ac-completion-face nil)
-(set-face-background 'hl-line-face "#1a1a1a")
+
+(require 'hl-line)
+(set-face-background hl-line-face "#0a0a0e")
+
+(require 'paren)
+(set-face-background 'show-paren-match-face (face-background 'default))
+(set-face-foreground 'show-paren-match-face "LightSeaGreen")
+(set-face-attribute 'show-paren-match-face nil :weight 'extra-bold)
+
+(require 'highlight-parentheses)
+(setq hl-paren-colors '("SandyBrown" "SaddleBrown" "RosyBrown"))
+
 (defcustom highlight-symbol-colors
   '("#2b2b2b" "#383838" "#4f4f4f" "#5f5f5f" "#6f6f6f")
   "*Colors used by `highlight-symbol-at-point'.
@@ -15,9 +36,6 @@ highlighting the symbols will use these colors in order."
   :group 'highlight-symbol)
 
 
-(setq mac-allow-anti-aliasing 1)
-(set-face-attribute 'default nil
-                    :family "DejaVu Sans Mono" :height 115 :weight 'normal)
 
 
 ;;(load-library "my-color-theme")
